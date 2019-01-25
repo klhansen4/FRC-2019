525f7c4aaf88b927bfe9d08f0ed1bd325b25425a
summershunia <BrianaBunny01@gmail.com>
2019-01-24 21:00:35 -0500

    worked on  climb commands and subsystems

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -34,19 +34,20 @@ public class RobotMap {
   // Climb Jack System Constants
   public static int climbJack_talon = 7;
   public static int ClimbJackFullyRetracted = 0;
-  public static int climbJackMaxExtend = 100000000; //not really lol TODO: figure out what  this number is
+  public static int climbJackMaxExtend = 21500; //not really lol TODO: figure out what  this number is
   public static int climbJackJogRetractedLimit = 0;
-  public static int climbJackJogExtendedLimit = 100000000; //not really lol TODO: figure out what  this number is
-  public static int climbJackJogExtendButton = 5;
-  public static int climbJackJogRetractButton = 6;
-  public static int climbJackFullyExtendButton = 7; //TODO: find out real button numbers 
-  public static int climbJackFullyRetractButton =  8; //TODO: find out real button numbers
+  public static int climbJackJogExtendedLimit = 21500; //not really lol TODO: figure out what  this number is
+  public static int climbJackJogExtendButton = 6; // right bumper
+  public static int climbJackJogRetractButton = 5; // left bumper
+  public static int climbJackFullyExtendButton = 3;   // button x 
+  public static int climbJackFullyRetractButton =  4; // button y
   public static int climbJackLimitSwitchExtendInput = 1;
   public static int climbJackLimitSwitchRetractInput = 2;
   public static double climbJackSpeed = 10.0;
   public static int climbJackRate = 200;
   public static int climbJackIsExtendedThreshold = 6000;
 
-
+  public final static int PID_PRIMARY = 0;
+  public final static int kTimeoutMs = 30;
 
 }
---
+++ src/main/java/frc/robot/subsystems/Climb_Jack.java
@@ -0,0 +1,118 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.RobotMap;
+import frc.robot.commands.JackJogExtendCommand;
+
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.*;
+
+
+/**
+ * Add your docs here.
+ */
+public class Climb_Jack extends Subsystem {
+  public static WPI_TalonSRX m_climbJack_talon = new WPI_TalonSRX (RobotMap.climbJack_talon);
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  private static DigitalInput m_limitSwitchExtended = new DigitalInput(RobotMap.climbJackLimitSwitchExtendInput);
+  private static DigitalInput m_limitSwitchRetract = new DigitalInput(RobotMap.climbJackLimitSwitchRetractInput);
+  int m_lastTargetPosition;
+  public Climb_Jack() {
+  
+    m_lastTargetPosition = getActualPosition();
+  }
+  public void periodic() {
+
+    //m_elevator_talon.config_kF(0,  SmartDashboard.getNumber("MotorKF", 0.0), 30); 
+    //m_elevator_talon.config_kP(0,  SmartDashboard.getNumber("MotorKp", 0.0), 30); 
+    //m_elevator_talon.config_kI(0,  SmartDashboard.getNumber("MotorKI", 0.0), 30); 
+    //m_elevator_talon.config_kD(0,  SmartDashboard.getNumber("MotorKD", 0.0), 30); 
+    //m_elevator_talon.configClosedloopRamp(SmartDashboard.getNumber("ElevatorRampRate",0.1),0);
+  
+    
+  
+    if (m_limitSwitchExtended.get() == false) {
+    m_climbJack_talon.getSensorCollection().setQuadraturePosition(0,0);
+    }
+    //SmartDashboard.putBoolean("ElevatorLimitSwitch", m_limitSwitchElevator.get());
+    SmartDashboard.putNumber("ClimbJackEncoderCounts",  getActualPosition());
+   
+    //SmartDashboard.putNumber("bullseyeElevatorPosition",  m_lastTargetPosition);
+  }
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new JackJogExtendCommand());
+  
+
+		m_climbJack_talon.configSelectedFeedbackSensor(	FeedbackDevice.QuadEncoder,				// Local Feedback Source
+
+		RobotMap.PID_PRIMARY,					// PID Slot for Source [0, 1]
+
+    RobotMap.kTimeoutMs);	
+    
+    m_climbJack_talon.configSensorTerm(SensorTerm.Sum0, FeedbackDevice.RemoteSensor0, RobotMap.kTimeoutMs);				// Feedback Device of Remote Talon
+
+    
+    m_climbJack_talon.configSensorTerm(SensorTerm.Sum1, FeedbackDevice.CTRE_MagEncoder_Relative, RobotMap.kTimeoutMs);	// Quadrature Encoder of current Talon		
+    m_climbJack_talon.configNominalOutputForward(0, 30); 
+    m_climbJack_talon.configNominalOutputReverse(0, 30); 
+    m_climbJack_talon.configPeakOutputForward(1, 30); 
+    m_climbJack_talon.configPeakOutputReverse(-1, 30); 
+    m_climbJack_talon.setSensorPhase(false);
+    m_climbJack_talon.config_kF(0, 0.0, 30); 
+    m_climbJack_talon.config_kP(0, 1.0, 30); 
+    m_climbJack_talon.config_kI(0, 0.0, 30); 
+    m_climbJack_talon.config_kD(0, 0.0, 30); 
+
+    //SmartDashboard.putNumber("MotorKF", 0.0); 
+    //SmartDashboard.putNumber("MotorKp", 1.0);
+    //SmartDashboard.putNumber("MotorKI", 0.0);
+    //SmartDashboard.putNumber("MotorKD", 0.0);
+
+    SmartDashboard.putNumber("ElevatorEncoderCounts", getActualPosition());
+
+    //SmartDashboard.putBoolean("ElevatorLimitSwitch", m_limitSwitchElevator.get());
+    //SmartDashboard.putNumber("ElevatorRampRate",0.1);
+  
+   
+    
+  }
+
+  public void setTargetPosition(int TargetPosition) {
+
+  if (TargetPosition < RobotMap.climbJackJogRetractedLimit)  {
+    TargetPosition = RobotMap.climbJackJogRetractedLimit;
+  }
+  if (TargetPosition > RobotMap.climbJackMaxExtend){
+    TargetPosition = RobotMap.climbJackMaxExtend;
+  }
+  m_climbJack_talon.set(ControlMode.Position, TargetPosition);
+    m_lastTargetPosition = TargetPosition;
+  }
+  public int getTargetPosition() {
+
+    return m_lastTargetPosition;
+     
+      }
+  
+      public int getActualPosition() {
+  
+        return m_climbJack_talon.getSensorCollection().getQuadraturePosition();
+         
+          }
+}
