514de4f65a466c9f7cb2bce40e9ba143e8b058ba
dakott@pobox.com <dakott@pobox.com>
2019-01-26 12:34:05 -0500

    Removed unused example subsystem and command. Fixed autoassistAlignment.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,8 +12,8 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
+
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Climb_Jack;
 
@@ -25,7 +25,7 @@ import frc.robot.subsystems.Climb_Jack;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+
   public static OI m_oi;
   public static Drive m_drive;
   public static Climb_Jack m_climb_jack;
@@ -41,7 +41,7 @@ public class Robot extends TimedRobot {
     m_oi = new OI();
     m_drive = new Drive();
     m_climb_jack = new Climb_Jack();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
